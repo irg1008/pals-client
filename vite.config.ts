3f222cd26058b27e6845dfe92312b5663e594a4f
irg1008 <ivansudevlop@gmail.com>
2023-08-16 18:22:19 +0200

    🔧 CONFIG: Update deps and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import eslint from "vite-plugin-eslint";
-import generouted from "@generouted/react-router/plugin";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import eslint from 'vite-plugin-eslint'
+import generouted from '@generouted/react-router/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
+	const env = loadEnv(mode, process.cwd(), '')
 
 	return {
 		plugins: [
@@ -14,23 +14,23 @@ export default defineConfig(({ mode }) => {
 			eslint(),
 			generouted({
 				source: {
-					routes: "./app/src/pages/**/[\\w[-]*.{jsx,tsx}",
-					modals: "./app/src/pages/**/[+]*.{jsx,tsx}",
+					routes: './app/src/pages/**/[\\w[-]*.{jsx,tsx}',
+					modals: './app/src/pages/**/[+]*.{jsx,tsx}'
 				},
-				output: "./app/src/router.ts",
-			}),
+				output: './app/src/router.ts'
+			})
 		],
-		root: "app",
+		root: 'app',
 		server: {
 			open: false,
-			proxy: { "/api": env.API_URL },
-			port: env.PORT,
+			proxy: { '/api': env.API_URL },
+			port: env.PORT
 		},
 		resolve: {
-			alias: { "@": path.resolve(__dirname, "/app/src") },
+			alias: { '@': path.resolve(__dirname, './app/src') }
 		},
 		define: {
-			__APP_ENV__: JSON.stringify(env.APP_ENV),
-		},
-	};
-});
+			__APP_ENV__: JSON.stringify(env.APP_ENV)
+		}
+	}
+})
