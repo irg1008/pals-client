d1bccdb7d92d0e89c33d9066189bd7a658323e90
irg1008 <ivansudevlop@gmail.com>
2023-08-25 20:27:03 +0200

    CONFIG: Update deps and minor configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,19 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: ['next/core-web-vitals', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	rules: {}
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: ['tailwindcss'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/classnames-order': 'off',
+    'prefer-const': 'error'
+  }
 }
